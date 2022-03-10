decb41a7f98d4c8dfb6a06c1def396621d4c09ba
oohhhhmyminds <zs.dawn@outlook.com>
2022-03-10 10:53:57 +0800

    124

---
+++ 58、调用calc.py
@@ -0,0 +1,3 @@
+# 项目时间：2022/3/10 10:20
+import calc
+print(calc.add(100,201))
---
+++ calc.py
@@ -0,0 +1,7 @@
+# 项目时间：2022/3/10 10:18
+def add(a, b):
+    return a + b
+
+
+if __name__ == '__main__':  # 只有当点击运行calc时才会计算 以主程序方式运行
+    print(add(10, 20))
