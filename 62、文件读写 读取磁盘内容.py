ba040b70049d95c50be9f8f5a42f2bb81f124e6c
oohhhhmyminds <zs.dawn@outlook.com>
2022-03-10 17:07:43 +0800

    129

--- 62、文件读写 读取磁盘内容.py
+++ 62、文件读写 读取磁盘内容.py
@@ -5,3 +5,25 @@
 file = open('1.txt', 'r')
 print(file.readline())
 file.close()
+file = open('2.txt', 'w')
+file.write('python')
+file.close()
+
+"""
+常见文件打开模式
+文件两大类：文本文件；二进制文件
+文本文件：存储的是普通字符文本，默认为Unicode字符集，可以使用记事本程序打开
+二进制文件：把数据内容用字节进行存储，无法使用记事本打开，需要使用专门的软件打开，如mp3、jpg
+r 只读
+w 只写 文件不存在则创建
+a 追加模式 不存在则创建
+b 二进制打开文件 不单独使用 与其他模式一起使用 如rb wb
++ 以读写方式打开文件 不单独使用 与其他模式一起 如a+
+"""
+src_file=open('jxau_logo.png','rb')
+
+target_file=open('copyjxau_logo.png','wb')
+target_file.write(src_file.read())
+
+target_file.close()
+src_file.close()
---
+++ 63、文件对象的常用方法.py
@@ -0,0 +1,28 @@
+# 项目时间：2022/3/10 16:35
+"""
+常用方法
+read([size])    从文件中读取size个字节或字符的内容返回，省略size则读取所有
+readline()      从文本文件中读取一行内容
+readlines()     把文本文件中每一行都作为独立的字符串对象，并放入列表返回
+write(str)      将字符串str内容写入文件
+writelines(s_list)  将字符串列表s_list写入文本文件，不添加换行符
+seek(offset,[,whence])  将文件指针移动到新的位置，offset表示相对于whence的位置：
+                            offset：为正往结束方向移动，为负往开始方向移动
+                            whence不同的数值代表不同的含义：
+                                0：从文件头开始计算（默认）
+                                1：从当前位置开始计算
+                                2：从文件尾开始计算
+tell()          返回文件指针的当前位置
+flush()         把缓冲区的内容写入文件，但不关闭文件
+close()         把缓冲区内容写入文件，同时关闭文件
+"""
+file=open('1.txt','r')
+# print(file.read(2))
+# print(file.readline())
+print(file.readlines())
+
+file=open('1.txt','a')
+file.write(' python')
+lst=['hello','java','hello']
+file.writelines(lst)
+file.close()
